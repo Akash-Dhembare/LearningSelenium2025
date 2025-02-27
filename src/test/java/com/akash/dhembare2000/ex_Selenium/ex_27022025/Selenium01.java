66be62ba0382956dab50bfed348e42829cb59973
Akash <akash.dhembare777@gmail.com>
2025-02-27 13:07:58 +0530

    .

--- src/test/java/com/akash/dhembare2000/ex_Selenium/ex_27022025/Selenium01.java
+++ src/test/java/com/akash/dhembare2000/ex_Selenium/ex_27022025/Selenium01.java
@@ -1,4 +1,11 @@
-package com.akash.dhembare2000.ex_Selenium;
+package com.akash.dhembare2000.ex_Selenium.ex_27022025;
+
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium01 {
+    public static void main(String[] args) {
+        ChromeDriver chromeDriver= new ChromeDriver();
+        chromeDriver.get("https://app.vwo.com/");
+
+    }
 }
